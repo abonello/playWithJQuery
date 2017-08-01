38efeaf7566196208ef27e4968ce1500b7cca903
abonello <ab@anthonybonello.co.uk>
2017-08-01 13:50:29 +0100

    Removed this variable

--- js/script2.js
+++ js/script2.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){ //  When document has finished loading run the following
     $(".box").on("click", function(){
-        var _this = this;
+        //var _this = this; //- this is not needed. 
         /*
         ** When we click on an element that has
         ** a 'box' class, its color will turn red.
@@ -9,9 +9,9 @@ $(document).ready(function(){ //  When document has finished loading run the fol
         var classNames = $(this).attr("class").split(" ");
         //All boxes having the same second class will turn red
         //$("." + classNames[1]).css("background-color", "red");
-        
+
         //Make it toggle
-        if ($(_this).css("background-color") === "rgb(255, 0, 0)") {
+        if ($(this).css("background-color") === "rgb(255, 0, 0)") {
             $("."+classNames[1]).css("background-color", "");
         }
         else {
